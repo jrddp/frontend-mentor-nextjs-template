f2a37db15e9be47ac022b09f8ea8c9b809686d16
Jared Peters <jar3dpt@gmail.com>
2024-09-18 18:01:27 -0400

    feat: preview mask

--- src/components/fm-preview/fm-preview-client.tsx
+++ src/components/fm-preview/fm-preview-client.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import { Blend, Columns2 } from "lucide-react";
 import { useState, useEffect } from "react";
 
 export function FMPreviewClient({ files }: { files: string[] }) {
-    const [selectedFile, setSelectedFile] = useState(files[0]);
+    const [selectedFile, setSelectedFile] = useState("");
     const [opacity, setOpacity] = useState(1);
     const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+    const [maskWidth, setMaskWidth] = useState(50);
 
     useEffect(() => {
         if (selectedFile) {
@@ -21,20 +23,36 @@ export function FMPreviewClient({ files }: { files: string[] }) {
     }, [selectedFile]);
 
     if (files.length === 0)
-        return <div className="absolute font-light bottom-2 left-2">Add design files to /design to preview.</div>;
+        return <div className="fixed bottom-0 left-0 z-50 p-4 font-light">Add design files to /public/design to preview.</div>;
 
     return (
         <>
             <div className="fixed bottom-0 left-0 z-50 flex flex-col gap-2 p-4 font-light">
                 {selectedFile && (
-                    <input
-                        type="range"
-                        min="0"
-                        max="1"
-                        step="0.01"
-                        value={opacity}
-                        onChange={(e) => setOpacity(parseFloat(e.target.value))}
-                    />
+                    <>
+                        <div className="flex items-center gap-1" >
+                            <Columns2 className="size-5" />
+                            <input className="w-full"
+                                type="range"
+                                min="0"
+                                max="100"
+                                step="1"
+                                value={maskWidth}
+                                onChange={(e) => setMaskWidth(parseInt(e.target.value))}
+                            />
+                        </div>
+                        <div className="flex items-center gap-1">
+                            <Blend className="size-5" />
+                            <input
+                                type="range"
+                                min="0"
+                                max="1"
+                                step="0.01"
+                                value={opacity}
+                                onChange={(e) => setOpacity(parseFloat(e.target.value))}
+                            />
+                        </div>
+                    </>
                 )}
                 <select className="bg-transparent"
                     value={selectedFile}
@@ -47,18 +65,23 @@ export function FMPreviewClient({ files }: { files: string[] }) {
                 </select>
             </div>
             {selectedFile && (
-                <div className="fixed inset-0 flex items-center justify-center">
-                    <img
-                        src={`/design/${selectedFile}`}
-                        style={{
-                            opacity: opacity,
-                            maxWidth: dimensions.width,
-                            maxHeight: dimensions.height
-                        }}
-                        alt="Design Preview"
-                    />
+                <div className="absolute flex items-center justify-center size-full pointer-events-none overflow-clip z-40">
+                    <div className="relative">
+                        <img
+                            src={`/design/${selectedFile}`}
+                            style={{
+                                opacity: opacity,
+                                maxWidth: dimensions.width,
+                                maxHeight: dimensions.height,
+                                clipPath: `polygon(${maskWidth}% 0, 100% 0, 100% 100%, ${maskWidth}% 100%)`
+                            }}
+                            alt="Design Preview"
+                        />
+                    </div>
                 </div>
             )}
+
+
         </>
     )
 }
