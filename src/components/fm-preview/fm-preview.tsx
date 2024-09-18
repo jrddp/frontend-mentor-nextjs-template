fc5146cc0638406b9f47a13c2b570e6f2b97eb60
Jared Peters <jar3dpt@gmail.com>
2024-09-18 18:21:24 -0400

    fix deployment

--- src/components/fm-preview/fm-preview.tsx
+++ src/components/fm-preview/fm-preview.tsx
@@ -3,6 +3,9 @@ import path from "path";
 import { FMPreviewClient } from "./fm-preview-client";
 
 export async function FMPreview() {
+    if (process.env.NODE_ENV !== 'development')
+        return null;
+
     const dir = path.join(process.cwd(), 'public', 'design');
     console.log(dir);
     const files = await fs.readdir(dir);
